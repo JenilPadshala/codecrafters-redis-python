f3f296ea440ed4181807370e4272a8d91d6a2a1f
Jenil Padshala <jenilpadshala@gmail.com>
2025-10-21 10:20:14 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -8,12 +8,15 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    connection, client_addr = server_socket.accept() # wait for client
     while(True):
-        connection, client_addr = server_socket.accept() # wait for client
-        with connection:
-            data = connection.recv(1024)
-            if data == b"*1\r\n$4\r\nPING\r\n":
-                connection.sendall(b"+PONG\r\n")
+        data = connection.recv(1024)
+        if data == b"*1\r\n$4\r\nPING\r\n":
+            connection.sendall(b"+PONG\r\n")
+        if not data:
+            print("No data received, closing connection.")
+            break        
+    connection.close()
 
 
 if __name__ == "__main__":
