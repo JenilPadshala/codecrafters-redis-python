939954c976c9abfee02f7e345003221b1d91abb4
Jenil Padshala <jenilpadshala@gmail.com>
2025-10-21 10:25:06 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -5,18 +5,25 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, client_addr = server_socket.accept() # wait for client
-    while(True):
-        data = connection.recv(1024)
-        if data == b"*1\r\n$4\r\nPING\r\n":
-            connection.sendall(b"+PONG\r\n")
-        if not data:
-            print("No data received, closing connection.")
-            break        
-    connection.close()
+    print("Server is listening on localhost:6379")
+
+    connection, client_addr = server_socket.accept()
+    print(f"Client connected: {client_addr}")
+
+    try:
+        while True:
+            data = connection.recv(1024)
+            if not data:
+                print("No data received, closing connection.")
+                break
+
+            if data == b"*1\r\n$4\r\nPING\r\n":
+                connection.sendall(b"+PONG\r\n")
+    finally:
+        connection.close()
+        print("Connection closed.")
+
 
 
 if __name__ == "__main__":
