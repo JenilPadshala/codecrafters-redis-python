6aef415084f33a89f4824071aa795a94f7cac025
Jenil Padshala <jenilpadshala@gmail.com>
2025-10-21 10:14:46 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -8,8 +8,12 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, client_addr = server_socket.accept() # wait for client
-    connection.sendall(b"+PONG\r\n")
+    while(True):
+        connection, client_addr = server_socket.accept() # wait for client
+        with connection:
+            data = connection.recv(1024)
+            if data == b"*1\r\n$4\r\nPING\r\n":
+                connection.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
