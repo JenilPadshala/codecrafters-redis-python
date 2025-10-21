2a2d3e06ef5b6ea45a1c8774c9bae3e07ce90704
Jenil Padshala <jenilpadshala@gmail.com>
2025-10-21 19:06:31 +0530

    implement key with an expiry

--- app/redis.py
+++ app/redis.py
@@ -37,7 +37,7 @@ class Redis:
     def get(self, key: str):
         if key in self.kv_store:
             record = self.kv_store[key]
-            if record["expire_at"] > datetime.now() or record["expire_at"].timestamp() == 0;
+            if record["expire_at"] > datetime.now() or record["expire_at"].timestamp() == 0:
                 return record["value"]
             else:
                 del self.kv_store[key]
