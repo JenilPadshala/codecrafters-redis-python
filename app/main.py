825115c65f59d55f292e4997e6175df4d9ff7e57
Jenil Padshala <jenilpadshala@gmail.com>
2025-10-21 11:07:21 +0530

    allow multiple connections using threading

--- app/main.py
+++ app/main.py
@@ -1,5 +1,20 @@
 import socket  # noqa: F401
+import threading
 
+BUF_SIZE = 1024
+
+def handle_client(client_socket: socket.socket, client_addr: tuple):
+    print(f"Client connected: {client_addr}")
+    try:
+        while True:
+            data = client_socket.recv(BUF_SIZE)
+            if not data:
+                print(f"No data received from {client_addr}, closing connection.")
+                break
+            if data == b"*1\r\n$4\r\nPING\r\n":
+                client_socket.sendall(b"+PONG\r\n")
+    finally:
+        client_socket.close()
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,21 +23,9 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     print("Server is listening on localhost:6379")
 
-    connection, client_addr = server_socket.accept()
-    print(f"Client connected: {client_addr}")
-
-    try:
-        while True:
-            data = connection.recv(1024)
-            if not data:
-                print("No data received, closing connection.")
-                break
-
-            if data == b"*1\r\n$4\r\nPING\r\n":
-                connection.sendall(b"+PONG\r\n")
-    finally:
-        connection.close()
-        print("Connection closed.")
+    while True:
+        client_socket, client_addr = server_socket.accept()
+        threading.Thread(target=handle_client, args=(client_socket, client_addr)).start()
 
 
 
