c18fc1890fb46c64732bbff762636600737b1188
Jenil Padshala <jenilpadshala@gmail.com>
2025-10-21 17:41:43 +0530

    implemented get and set commands

--- app/redis.py
+++ app/redis.py
@@ -7,8 +7,12 @@ class Redis:
         command = command.upper()
         if command == "PING":
             return self.ping()
-        elif command == "ECHO":
-            return self.echo(*args)
+        elif command == "ECHO" and args:
+            return self.echo(args[0])
+        elif command == "SET" and len(args) == 2:
+            return self.set(args[0], args[1])
+        elif command == "GET" and len(args) == 1:
+            return self.get(args[0])
     
     # ---- Command Implementations ----
     def ping(self):
@@ -17,3 +21,9 @@ class Redis:
     def echo(self, msg: str):
         return msg
     
+    def set(self, key: str, value: str):
+        self.kv_store[key] = value
+        return "OK"
+    
+    def get(self, key: str):
+        return self.kv_store.get(key, None)
