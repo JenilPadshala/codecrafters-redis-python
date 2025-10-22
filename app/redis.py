587289fd05cda463a64d0348831ae88005d8918c
Jenil Padshala <jenilpadshala@gmail.com>
2025-10-22 11:03:27 +0530

    implementing BLPOP

--- app/redis.py
+++ app/redis.py
@@ -160,8 +160,5 @@ class Redis:
                 return self.lpop(key)
             if timeout_sec > 0 and datetime.now(timezone.utc) >= timeout_limit:
                 return None
-            
-            # Sleep for a short duration to avoid busy waiting
-            time.sleep(0.1)
 
         
