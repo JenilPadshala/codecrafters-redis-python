63746793daf292c06ac3841e4cf8c94e9084d2b1
Jenil Padshala <jenilpadshala@gmail.com>
2025-10-23 08:46:02 +0530

    Implement XREAD Command with multiple streams

--- app/redis.py
+++ app/redis.py
@@ -276,29 +276,6 @@ class Redis:
             return NullArray()
         
         return results
-            
-    # def xread(self, stream_opt: str, key: str, id: str) -> Union[deque, NullArray]:
-    #     """Read stream entries with ID greater than the given ID."""
-        
-    #     if key not in self.stream_store:
-    #         return NullArray()
-        
-    #     #parse the given ID
-    #     given_id = self._parse_id(id)
-    #     stream = self.stream_store[key]
-
-    #     entries = deque()
-    #     for entry in stream:
-    #         entry_id = self._parse_id(entry["id"])
-    #         if entry_id[0] > given_id[0] or (entry_id[0] == given_id[0] and entry_id[1] > given_id[1]):
-    #             entries.append([entry["id"], [item for pair in entry["data"].items() for item in pair]])
-
-    #     if not entries:
-    #         return NullArray()
-        
-    #     result = deque([[key, entries]])
-
-    #     return result
 
     
     # ---- Helper Functions ----
